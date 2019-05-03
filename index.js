05e3076fb42629bdee039fac51f6a728f3222e5e
Travis Wickham <twickham37@gmail.com>
2019-05-02 22:06:52 -0500

    Initial js and html; packages

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    plugins: [ 'dayGrid' ]
+  });
+
+  calendar.render();
+});
